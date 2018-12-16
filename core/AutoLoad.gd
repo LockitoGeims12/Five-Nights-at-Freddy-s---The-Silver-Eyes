5941a598719e4ecb5f076d291a6ab0723c586b28
JulioYagami <JulioHenrique31501234@hotmail.com>
2018-12-16 19:49:27 -0300

    Add language support

---
+++ core/AutoLoad.gd
@@ -0,0 +1,22 @@
+extends Node
+
+const GAME_NAME = "Five Nights at Freddy's - The Silver Eyes"
+const GAME_VERSION = "0.0.1"
+
+var LANG = {
+	"NEWGAME": "",
+	"OPTIONS": "",
+	"EXTRAS": "",
+	"QUIT": "",
+	"UNKNOWN": ""
+	}
+var GameLanguage = "en"
+
+func _ready():
+	var file = File.new()
+	file.open("res://languages/" + GameLanguage + ".txt", file.READ)
+	LANG.NEWGAME = file.get_line()
+	LANG.OPTIONS = file.get_line()
+	LANG.EXTRAS = file.get_line()
+	LANG.QUIT = file.get_line()
+	file.close()
---
+++ interface/VersionIndicator.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready():
+	$VersionText.text = "Version: " + AutoLoad.GAME_VERSION
