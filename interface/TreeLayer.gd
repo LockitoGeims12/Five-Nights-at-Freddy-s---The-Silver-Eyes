e10c5d2f5eb9e3407a4576fee54ecf56c11b0d10
JulioYagami <JulioHenrique31501234@hotmail.com>
2018-12-17 07:23:38 -0300

    TreeLayer is completely implemmented

--- interface/TreeLayer.gd
+++ interface/TreeLayer.gd
@@ -1,13 +1,25 @@
-extends Node2D
+extends Sprite
 
 export(int) var LayerSpeed = 200
-export(Vector2) var TimerRange = Vector2(0, 0)
+export(Vector2) var TimerRange = Vector2(3, 4)
+var ThisTimer = Timer.new()
 
 func _ready():
-	pass
+	randomize()
+	ThisTimer.connect("timeout", self, "_on_check_layer_position")
+	_randomize()
+	ThisTimer.one_shot = false
+	ThisTimer.autostart = true
+	add_child(ThisTimer)
 
 func _on_check_layer_position():
 	if global_position.x <= -1024:
 		global_position.x = 1024
+	_randomize()
+
+func _process(delta):
+	translate(Vector2(-1, 0) * LayerSpeed * delta)
+
+func _randomize():
 	randomize()
-	$Timer.wait_time = rand_range(TimerRange.x, TimerRange.y)
+	ThisTimer.wait_time = rand_range(TimerRange.x, TimerRange.y)
--- interface/TreeLayer2.gd
+++ interface/TreeLayer2.gd
@@ -1,12 +1,5 @@
-extends Sprite
+extends "res://interface/TreeLayer.gd"
 
-var LayerSpeed = 200
-
-func _process(delta):
-	translate(Vector2(-1, 0) * LayerSpeed * delta)
-
-func _on_check_layer_position():
-	if global_position.x <= -1024:
-		global_position.x = 1024
-	randomize()
-	$Timer.wait_time = rand_range(5, 7)
+func _ready():
+	LayerSpeed = 300
+	TimerRange = Vector2(6, 8)
