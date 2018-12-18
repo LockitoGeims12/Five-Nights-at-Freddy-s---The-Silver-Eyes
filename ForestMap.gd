8efe00897951c95ed5488dfc0d564bd17c6a40a2
JulioYagami <JulioHenrique31501234@hotmail.com>
2018-12-18 09:22:35 -0300

    Update

---
+++ ForestMap.gd
@@ -0,0 +1,4 @@
+extends "res://maps/Map.gd"
+
+func _ready():
+	_set_lighting(LIGHTINGTYPES.DARK)
--- interface/GameTitle.gd
+++ interface/GameTitle.gd
@@ -1,10 +1,16 @@
 extends Label
 
-func _ready():
-	pass
+var MouseIsOverTitle = false
+
+func _input(event):
+	if event.is_action_pressed("mouse_left_button"):
+		var i = preload("res://interface/Credits.tscn")
+		get_tree().change_scene_to(i)
 
 func _on_Area2D_mouse_entered():
+	MouseIsOverTitle = true
 	$TitleAnimation.play("FocusIn")
 
 func _on_Area2D_mouse_exited():
+	MouseIsOverTitle = false
 	$TitleAnimation.play("FocusOut")
