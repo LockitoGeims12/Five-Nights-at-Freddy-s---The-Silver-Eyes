91016e3109b766c3cee2c0bedac5beb78112ad16
JulioYagami <JulioHenrique31501234@hotmail.com>
2018-12-20 19:12:42 -0300

    Added xp function to RPGMath.gd

--- interface/GameTitle.gd
+++ interface/GameTitle.gd
@@ -1,8 +1,7 @@
 extends Label
 
 func _on_Button_button_down():
-	var i = preload("res://interface/Credits.tscn")
-	get_tree().change_scene_to(i)
+	get_tree().change_scene("res://interface/Credits.tscn")
 
 func _on_Button_mouse_entered():
 	$TitleAnimation.play("FocusIn")
--- utils/RPG/RPGMath.gd
+++ utils/RPG/RPGMath.gd
@@ -1,8 +1,9 @@
 extends Node
 
-func _ready():
-	pass
-
 func _damage(attack, defense):
 	var damage = attack * ( 100.0 / (100.0 + defense))
-	return damage
+	return damage # calculates damage and returns it
+
+func _xp(playerLevel, enemyLevel):
+	var xp = (playerLevel * enemyLevel) * 100 + (enemyLevel - playerLevel) * 100
+	return xp # calculates won xp and returns it
