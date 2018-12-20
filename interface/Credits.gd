bbd47f9cd6f17c40ebc339896066562e345559f9
JulioYagami <JulioHenrique31501234@hotmail.com>
2018-12-20 16:01:42 -0300

    Credits updated, escape input added

--- interface/Credits.gd
+++ interface/Credits.gd
@@ -1,4 +1,8 @@
 extends Node
 
-func _ready():
-	pass
+func _input(event):
+	if event.is_action_pressed("escape"):
+		_backToMenu()
+
+func _backToMenu():
+	get_tree().change_scene("res://interface/Menu.tscn")
