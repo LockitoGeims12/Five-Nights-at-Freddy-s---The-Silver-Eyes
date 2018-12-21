bd86cd6f806c524b6d0f816adfd041f694619861
JulioYagami <JulioHenrique31501234@hotmail.com>
2018-12-21 08:35:00 -0300

    Update RPGMath.gd

--- utils/RPG/RPGMath.gd
+++ utils/RPG/RPGMath.gd
@@ -4,6 +4,10 @@ func _damage(attack, defense):
 	var damage = attack * ( 100.0 / (100.0 + defense))
 	return damage # calculates damage and returns it
 
+func _nextLevelXp(currentLevel):
+	var xp = (currentLevel * 500) + (currentLevel * 250)
+	return xp # calculates the xp to the next level
+
 func _xp(playerLevel, enemyLevel):
 	var xp = (playerLevel * enemyLevel) * 100 + (enemyLevel - playerLevel) * 100
-	return xp # calculates won xp and returns it
+	return xp # calculates won xp when the player won a monster and returns it
