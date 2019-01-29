303acfce480ac98502d53cfa5ed1b1a8f84961f1
JulioYagami <33421921+JulioYagami@users.noreply.github.com>
2019-01-29 15:22:00 -0300

    HUDHint.gd update

--- interface/HUDHint.gd
+++ interface/HUDHint.gd
@@ -5,7 +5,3 @@ export(int) var duration = 5
 func _ready():
 	$AnimationPlayer.playback_speed = 1.0 / duration
 	$AnimationPlayer.play("Hide")
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "Hide":
-		queue_free()
