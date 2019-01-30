11a3a42b9eef1fc68213b865cd9543e8646cf0db
JulioYagami <33421921+JulioYagami@users.noreply.github.com>
2019-01-30 18:01:47 -0300

    WelcomeScreen.gd

--- interface/WelcomeScreen.gd
+++ interface/WelcomeScreen.gd
@@ -1,5 +1,11 @@
-tool
 extends Node2D
+
+func _disable_input():
+	set_process_input(false)
+
+func _input(event):
+	if event is InputEventKey or event is InputEventMouseButton or event is InputEventJoypadButton:
+		$AnimationPlayer.play("Hide")
 
 func _draw():
 	draw_rect(Rect2(0, 0, 1024, 768), Color(0, 0, 0, 0.75))
