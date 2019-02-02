efe6265ae6e74052c8613ae25fb8189e571e05b1
JulioYagami <33421921+JulioYagami@users.noreply.github.com>
2019-02-02 16:49:52 -0300

    Update Menu.gd

--- interface/Menu.gd
+++ interface/Menu.gd
@@ -2,14 +2,14 @@ extends Node
 
 func _ready():
 	if not AutoLoad.welcome_shown:
-		var _res = AutoLoad.RESOURCES.WELCOME_SCREEN
-		var _instance = _res.instance()
-		add_child(_instance)
+		var _myClass = AutoLoad.RESOURCES.WELCOME_SCREEN
+		var _myInstance = _myClass.instance()
+		add_child(_myInstance)
 		AutoLoad.welcome_shown = true
 
 func _input(event):
 	# NOTE: this input method is temporary
-	if event is InputEventJoypadButton:
+	if AutoLoad.welcome_shown && event is InputEventJoypadButton:
 		if Input.is_joy_button_pressed(0, JOY_SONY_TRIANGLE):
 			$NewGameButton._on_Button_mouse_down()
 		elif Input.is_joy_button_pressed(0, JOY_SONY_CIRCLE):
